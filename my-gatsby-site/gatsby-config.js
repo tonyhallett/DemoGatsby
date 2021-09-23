428e84be380b85bee7037c9e91dca3142db1c497
Tony Hallett <tonyhallett74@gmail.com>
2021-09-23 15:56:41 +0100

    add gatsby site

---
+++ my-gatsby-site/gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "My Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
