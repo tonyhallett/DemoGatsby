4883bd9af4cb88d10510934bdc9944eb0c62c0f7
Tony Hallett <tonyhallett74@gmail.com>
2021-10-06 15:24:57 +0100

    Updates

---
+++ component---src-pages-match-match-js-07f43f04f1a56799a074.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[637],{6813:function(e,t,a){a.r(t);var n=a(7294);t.default=function(e){return n.createElement("main",null,n.createElement("title",null,"Match page"),n.createElement("div",null,"This is a match page"))}}}]);
+//# sourceMappingURL=component---src-pages-match-match-js-07f43f04f1a56799a074.js.map
